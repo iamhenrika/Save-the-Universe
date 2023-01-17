95d353f9c1aea96f2791fa78f0f975650bacabd0
iamhenrika <113649340+iamhenrika@users.noreply.github.com>
2023-01-17 11:14:09 -0500

    Update

---
+++ script.js
@@ -0,0 +1,72 @@
+class Ship {
+  alien = false;
+  constructor(type) {
+    if (type == "alien") {
+      this.alien = true;
+      this.hull = Math.floor(Math.random() * 4) + 3;
+      this.firepower = Math.floor(Math.random() * 3) + 2;
+      this.accuracy = Math.random() * 0.2 + 0.6;
+    } else {
+      this.hull = 20;
+      this.firepower = 5;
+      this.accuracy = 0.7;
+    }
+    this.display();
+  }
+  shootAt(targetShip) {
+    if (Math.random() < this.accuracy) {
+      targetShip.gotHitBy(this);
+    }
+  }
+  gotHitBy(attacker) {
+    this.hull -= attacker.firepower;
+    this.display();
+  }
+  display() {
+    let hullHTML, fpHTML;
+    if (this.alien) {
+      hullHTML = document.querySelector("#alienHull");
+      fpHTML = document.querySelector("#alienFP");
+    } else {
+      hullHTML = document.querySelector("#myHull");
+      fpHTML = document.querySelector("#myFP");
+    }
+    hullHTML.innerHTML = "Hull:" + this.hull;
+    fpHTML.innerHTML = `FP:${this.firepower}`;
+  }
+}
+
+let battleShip;
+let alien;
+
+const shootBtn = document.querySelector("#shootBtn");
+const retreatBtn = document.querySelector("#retreatBtn");
+document.querySelector("#startBtn").addEventListener("click", () => {
+  battleShip = new Ship();
+  alien = new Ship("alien");
+  shootBtn.disabled = false;
+  retreatBtn.disabled = false;
+});
+shootBtn.addEventListener("click", () => {
+  battleShip.shootAt(alien);
+  if (alien.hull > 0) {
+    alien.shootAt(battleShip);
+    if (battleShip.hull <= 0) {
+      alert("You lost!");
+      gameOver();
+    }
+  } else {
+    alert("You win!");
+    gameOver();
+  }
+});
+
+retreatBtn.addEventListener("click", () => {
+  alert("You retreated!");
+  gameOver();
+});
+
+function gameOver() {
+  shootBtn.disabled = true;
+  retreatBtn.disabled = true;
+}
